0b5fa86048146eaf5b1f8bdbeabbda7c82a8ea65
Abhishek SAINI <saini.abhishek@hotmail.com>
2025-01-26 14:59:17 +0100

    feat: create default spinner loader with text

---
+++ app/components/Spinner/WithText.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex justify-center items-center space-x-4">
+    <div class="w-16 h-16 border-4 border-t-transparent border-blue-700  border-solid rounded-full animate-spin" />
+    <span class="text-lg text-slate-700 dark:text-slate-300">Loading...</span>
+  </div>
+</template>
