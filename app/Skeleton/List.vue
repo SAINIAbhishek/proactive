da338843b441891432a63bd97afac7d6d733ef02
Abhishek SAINI <saini.abhishek@hotmail.com>
2025-01-26 13:16:44 +0100

    feat: create skeleton Card and List components for loading effect
    - Created skeleton `Card` and `List` components to display loading placeholders while data is being fetched.
    - Incorporated the global `Card` and `CardList` components to maintain a consistent layout and design.

---
+++ app/Skeleton/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <CardList>
+    <template #image>
+      <div class="bg-slate-100 h-full w-full dark:bg-gray-300 rounded-lg">
+        <USkeleton class="h-full w-full" />
+      </div>
+    </template>
+    <template #title>
+      <div class="flex-1 space-y-4">
+        <div class="h-8 bg-slate-100 dark:bg-gray-300 rounded-lg">
+          <USkeleton class="h-full w-full" />
+        </div>
+        <div class="h-6 bg-slate-100 dark:bg-gray-300 rounded-lg w-3/4">
+          <USkeleton class="h-full w-full" />
+        </div>
+      </div>
+    </template>
+  </CardList>
+</template>
