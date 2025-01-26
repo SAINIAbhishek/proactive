0b44ee1ed66946caa427d5d9662697c31008770a
Abhishek SAINI <saini.abhishek@hotmail.com>
2025-01-26 15:13:26 +0100

    feat: add validateLimitQueryParam helper functions
    - Added a new `validateLimitQueryParam` helper function to validate the limit query parameter.

--- app/helpers/validatePageParams.ts
+++ app/helpers/validatePageParams.ts
@@ -1,4 +1,4 @@
-import { defaultPageNumber } from '~/constants'
+import { defaultItemsPerPage, defaultPageNumber } from '~/constants'
 
 /**
  * Validates the page query parameter and falls back to the default page number if invalid.
@@ -8,4 +8,14 @@ import { defaultPageNumber } from '~/constants'
 export function validatePageQueryParam(page: string | undefined): number {
   const parsedPage = Number.parseInt(page || defaultPageNumber.toString(), 10)
   return Number.isNaN(parsedPage) || parsedPage < 1 ? defaultPageNumber : parsedPage
+}
+
+/**
+ * Validates the limit query parameter and falls back to the default items per page if invalid.
+ * @param limit - The limit query parameter from the route
+ * @returns The validated items per page limit
+ */
+export function validateLimitQueryParam(limit: string | undefined): number {
+  const parsedLimit = Number.parseInt(limit || defaultItemsPerPage.toString(), 10)
+  return Number.isNaN(parsedLimit) || parsedLimit <= 0 ? defaultItemsPerPage : parsedLimit
 }
