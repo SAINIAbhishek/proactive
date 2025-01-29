4a8e02d6265085fb1f31296f1ab3bbf60f238909
Abhishek SAINI <saini.abhishek@hotmail.com>
2025-01-29 16:49:23 +0100

    Update Header.vue

--- app/components/Application/Header/Header.vue
+++ app/components/Application/Header/Header.vue
@@ -6,14 +6,8 @@
           <li>
             <ApplicationHeaderLink to="/" label="Home" />
           </li>
-          <li>
-            <ApplicationHeaderLink to="/assignment" label="Read the assignment instructions" />
-          </li>
         </ul>
       </nav>
-      <div class="ml-auto">
-        <UButton to="https://github.com/spend-cloud-tom/front-end-boilerplate" icon="mdi:github" color="blue" label="GitHub repository" variant="outline" />
-      </div>
     </UContainer>
   </header>
 </template>
